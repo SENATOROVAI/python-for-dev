aec954c205cf4dc3e46b0a1ed273d34cfa762453
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2025-04-16 21:18:16 +0300

    Update error_code.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- error_code.py
+++ error_code.py
@@ -1,5 +1,29 @@
-def say_hello(name):
-    print("Hello, " + name)
+import os, sys
+
+x = 42
 
 
-say_hello("Bob")
+def myFunc(x, y):
+    return x + y
+
+
+def Unused():
+    pass
+
+
+class test:
+    def __init__(self, value):
+        self.Value = value
+
+    def printvalue(self):
+        print("The value is: " + str(self.Value))
+
+
+def main():
+    result = myFunc(x, 5)
+    print(result)
+    t = test(10)
+    t.printvalue()
+
+
+main()
